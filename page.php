585b0006d2f0432358ff1a71d8c700829368876f
Matthew Duhan <cjfringe@gmail.com>
2015-11-28 16:48:56 -0600

    version 0.0.3; bg on section

--- page.php
+++ page.php
@@ -22,29 +22,31 @@
 								</header> <?php // end article header ?>
 
 								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
+									<div class="inner-entry-content cf">
+										<?php
+											// the content (pretty self explanatory huh)
+											the_content();
 
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
+											/*
+											 * Link Pages is used in case you have posts that are set to break into
+											 * multiple pages. You can remove this if you don't plan on doing that.
+											 *
+											 * Also, breaking content up into multiple pages is a horrible experience,
+											 * so don't do it. While there are SOME edge cases where this is useful, it's
+											 * mostly used for people to get more ad views. It's up to you but if you want
+											 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+											 *
+											 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+											 *
+											*/
+											wp_link_pages( array(
+												'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+												'after'       => '</div>',
+												'link_before' => '<span>',
+												'link_after'  => '</span>',
+											) );
+										?>
+									</div>
 								</section> <?php // end article section ?>
 
 								<footer class="article-footer cf">
