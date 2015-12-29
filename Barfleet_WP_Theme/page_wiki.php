f36c441c35f796c5c356e9ffe5ab18dc078e0023
Matthew Duhan <cjfringe@gmail.com>
2015-12-29 16:28:18 -0600

    Updating wiki template

--- Barfleet_WP_Theme/page_wiki.php
+++ Barfleet_WP_Theme/page_wiki.php
@@ -1,6 +1,6 @@
 <?php
 /*
- Wiki: Custom Wiki Page
+ Template Name: Custom Wiki Page
  *
  * This is your custom page template. You can create as many of these as you need.
  * Simply name is "page-whatever.php" and in add the "Template Name" title at the
@@ -31,37 +31,39 @@
 
 									<h1 class="page-title"><?php the_title(); ?></h1>
 
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
-
-
 								</header>
 
 								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
+									<div class="inner-entry-content cf">
 
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
+										<div class="byline vcard">
+											<?php printf( __( 'Bar-Date', 'bonestheme').' <time class="updated" datetime="%1$s" itemprop="datePublished">%3$s%4$s</time> '.__( 'by',  'bonestheme').' <span class="author">%5$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), substr(get_the_time('Y'), 0, 1), substr(get_the_time('Ym.d.H.i'), 2), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
+										</div>
+
+										<?php
+											// the content (pretty self explanatory huh)
+											the_content();
+
+											/*
+											 * Link Pages is used in case you have posts that are set to break into
+											 * multiple pages. You can remove this if you don't plan on doing that.
+											 *
+											 * Also, breaking content up into multiple pages is a horrible experience,
+											 * so don't do it. While there are SOME edge cases where this is useful, it's
+											 * mostly used for people to get more ad views. It's up to you but if you want
+											 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+											 *
+											 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+											 *
+											*/
+											wp_link_pages( array(
+												'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+												'after'       => '</div>',
+												'link_before' => '<span>',
+												'link_after'  => '</span>',
+											) );
+										?>
+									</div>
 								</section>
 
 
