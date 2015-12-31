8bd04a6b74f2744a214f41950dcb10aa80d47793
Matthew Duhan <cjfringe@gmail.com>
2015-12-30 22:49:35 -0600

    Custom events page (no bardate). May also remove sidebar.

---
+++ Barfleet_WP_Theme/page-events.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ Template Name: Custom Events page (no Bardate)
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div class="content-lcars lcars-left"></div>
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+								<header class="article-header">
+
+									<h1 class="page-title"><?php the_title(); ?></h1>
+
+								</header>
+
+								<section class="entry-content cf" itemprop="articleBody">
+									<div class="inner-entry-content cf">
+
+										<?php
+											// the content (pretty self explanatory huh)
+											the_content();
+
+											/*
+											 * Link Pages is used in case you have posts that are set to break into
+											 * multiple pages. You can remove this if you don't plan on doing that.
+											 *
+											 * Also, breaking content up into multiple pages is a horrible experience,
+											 * so don't do it. While there are SOME edge cases where this is useful, it's
+											 * mostly used for people to get more ad views. It's up to you but if you want
+											 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+											 *
+											 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+											 *
+											*/
+											wp_link_pages( array(
+												'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+												'after'       => '</div>',
+												'link_before' => '<span>',
+												'link_after'  => '</span>',
+											) );
+										?>
+									</div>
+								</section>
+
+
+								<footer class="article-footer">
+
+                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+
+								</footer>
+
+								<?php comments_template(); ?>
+
+							</article>
+
+							<?php endwhile; else : ?>
+
+									<article id="post-not-found" class="hentry cf">
+											<header class="article-header">
+												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										</header>
+											<section class="entry-content">
+												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+						</main>
+
+						<?php get_sidebar(); ?>
+
+				</div>
+				<div class="content-lcars lcars-right"></div>
+
+			</div>
+
+
+<?php get_footer(); ?>
